efe989fae61d2a04875e27f2be427c75dc08b5e4
neevek <i@neevek.net>
2018-07-16 18:44:14 +0800

    comment out unused code

--- src/util.hpp
+++ src/util.hpp
@@ -154,43 +154,43 @@ namespace uvcpp {
         return ip[15] == 1;
       }
 
-      static int doSetUID(const char *user) {
-        const std::size_t pwd_buf_size = 8192;
-        struct passwd pwd, *result;
-        char pwd_buf[pwd_buf_size];
-        int status = getpwnam_r(user, &pwd, pwd_buf, pwd_buf_size, &result);
-        if (result == NULL) {
-          if (status > 0) {
-            LOG_E("getpwnam_r failed: %s", strerror(errno));
-          } else {
-            LOG_E("user %s not found", user);
-          }
-          return -1;
-        }
+      //static int doSetUID(const char *user) {
+        //const std::size_t pwd_buf_size = 8192;
+        //struct passwd pwd, *result;
+        //char pwd_buf[pwd_buf_size];
+        //int status = getpwnam_r(user, &pwd, pwd_buf, pwd_buf_size, &result);
+        //if (result == NULL) {
+          //if (status > 0) {
+            //LOG_E("getpwnam_r failed: %s", strerror(errno));
+          //} else {
+            //LOG_E("user %s not found", user);
+          //}
+          //return -1;
+        //}
 
-        int euid = geteuid();
-        int egid = getegid();
+        //int euid = geteuid();
+        //int egid = getegid();
 
-        status = setegid(pwd.pw_gid);
-        status = seteuid(pwd.pw_uid);
-        if (status == EINVAL || status == EPERM) {
-          LOG_E("setegid or seteuid failed");
-          return -1;
-        }
+        //status = setegid(pwd.pw_gid);
+        //status = seteuid(pwd.pw_uid);
+        //if (status == EINVAL || status == EPERM) {
+          //LOG_E("setegid or seteuid failed");
+          //return -1;
+        //}
 
-        LOG_I("switched egid from %d to %d, euid from %d to %d", 
-            egid, getegid(), euid, geteuid());
-        return 0;
-      }
+        //LOG_I("switched egid from %d to %d, euid from %d to %d", 
+            //egid, getegid(), euid, geteuid());
+        //return 0;
+      //}
 
-      static void redirect_stderr_to_file(const char *log_file) {
-        FILE *old_stderr = stderr;
-        stderr = fopen(log_file, "w");
-        if (stderr == NULL) {
-          fprintf(old_stderr, "failed to open log file: %s", log_file);
-          exit(1);
-        }
-      }
+      //static void redirect_stderr_to_file(const char *log_file) {
+        //FILE *old_stderr = stderr;
+        //stderr = fopen(log_file, "w");
+        //if (stderr == NULL) {
+          //fprintf(old_stderr, "failed to open log file: %s", log_file);
+          //exit(1);
+        //}
+      //}
 
   };
 
